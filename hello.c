cd1e3368f8c071ccfb4694c3d40b6a9581ae80d5
alarson <alarson22@huskers.unl.edu>
2020-08-19 22:10:41 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Andrew Larson
+ * Date: Aug 18 2020
+ *
+ * Hello world program
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+
+     printf("Hello world!\n");
+
+     return 0;
+
+ }
